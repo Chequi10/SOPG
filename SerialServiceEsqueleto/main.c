dd8307a781aafab75448bd1a4919c5d4cb91653e
Chequi10 <acerboezequiel@live.com>
2022-06-18 12:19:57 -0300

    segundo commit

--- SerialServiceEsqueleto/main.c
+++ SerialServiceEsqueleto/main.c
@@ -8,6 +8,10 @@ int main(void)
 {
 	printf("Inicio Serial Service\r\n");
 	
+	if(serial_open(1,115200)!=0)
+		{
+			printf("Error abriendo puerto serie\r\n");
+		}
 	
 	exit(EXIT_SUCCESS);
 	return 0;
